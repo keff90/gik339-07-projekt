3d5b97a7675880f7d3850d0c93c264c1c774ddc2
h23pothi <h23pothi@du.se>
2024-12-19 13:52:20 +0100

    Skapat filer samt installera npm, express och sqlite3

---
+++ Projekt/Server/server.js
@@ -0,0 +1,17 @@
+const sqlite = require('sqlite3').verbose();
+const db = new sqlite.Database('./katt.db');
+
+const express = require('express');
+const server = express();
+
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    
+    next();
+  });

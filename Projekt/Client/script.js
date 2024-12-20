2a94e46cf0da4aabcaaab8fa94f8d50f443e5d78
h23linla <h23linla@du.se>
2024-12-20 17:02:49 +0100

    Formuläret (forts.)

--- Projekt/Client/script.js
+++ Projekt/Client/script.js
@@ -38,6 +38,23 @@ function fetchData() {
 });
 };
 
+console.log(userForm);
+userForm.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const serverUserObject = {
+        godisName: "",
+        color: "",
+        price: "",
+    };
+    serverUserObject.godisName = userForm.godisName.value;
+    serverUserObject.color = userForm.color.value;
+    serverUserObject.price = userForm.price.value;
+
+    console.log(serverUserObject);
+}
+
 /*fetch(url)
 .then((response) => response.json())
 .then((godis) => {

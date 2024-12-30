1c47eafef69b27ba1e2f841c54e9827cc1b74315
h23pothi <h23pothi@du.se>
2024-12-30 14:44:38 +0100

    Skrivit Post, put och delete

--- Projekt/Server/server.js
+++ Projekt/Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 const sqlite3 = require('sqlite3').verbose();
-
+const db = new sqlite3.Database('./godis.db');
 
 server
   .use(express.json())
@@ -19,7 +19,6 @@ server
   });
 
   server.get('/godis', (req, res) => {
-    const db = new sqlite3.Database('./godis.db');
     const sql = 'SELECT * FROM godis';
 
     db.all(sql, (err, rows) => {
@@ -29,4 +28,60 @@ server
         res.send(rows);
       }
     });
+  });
+
+  server.post('/godis', (req, res) => {
+    const godis = req.body;
+    const sql = `INSERT INTO godis(godisName, color, price) VALUES (?,?,?)`;
+
+    db.run(sql, Object.values(godis), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send('Godis sparades');
+      }
+    });
+  });
+
+  server.put('/godis', (req, res) => {
+    const bodyData = req.body;
+    
+    const id = bodyData.id;
+    const godis = {
+      godisName: bodyData.godisName, 
+      color: bodyData.color, 
+      price: bodyData.price
+    }
+
+    let updateString = "";
+    const columnsArray = Object.keys(godis);
+    columnsArray.forEach((column, i) => {
+      updateString += `${column}="${godis[column]}`;
+      if(i != columnsArray.length - 1) updateString += ",";
+    });
+    const sql = `UPDATE godis SET ${updateString} WHERE id=${id}`;
+
+    db.run(sql, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send('Godis uppdaterades');
+      }
+    });
+  });
+
+  server.delete('/godis/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM godis WHERE id = ${id}`;
+    
+    db.run(sql, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err);
+      } else {
+        res.send('Godis bort tagen');
+      }
+    })
   });
